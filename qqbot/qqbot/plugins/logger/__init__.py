8b125381fc7a051118ea317a85d386ed21236393
你的 Github 用户名 < Github >
2024-12-22 19:30:58 +0800

    logger

--- qqbot/qqbot/plugins/logger/__init__.py
+++ qqbot/qqbot/plugins/logger/__init__.py
@@ -14,5 +14,12 @@ trigger = on_message()
 async def handle_keyword(bot: Bot, event: Event, state: T_State):
     message = str(event.get_message())
     user_id = event.get_user_id()
+    user_name = event.get_user_name()
+    group_id = event.group_id if hasattr(event, 'group_id') else None
+    if group_id:
+        group_name = await bot.get_group_info(group_id=group_id)['group_name']
+    else:
+        group_name = None
+    logger.info(f"群聊: {group_name}({group_id})")
+    logger.info(f"用户: {user_name}({user_id})")
     logger.info(f"消息: {message}")
-    logger.info(f"消息来源: {user_id}")
